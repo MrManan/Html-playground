fb5e16aa1486df0874b2f205e1c04bfcdd1dacae
MR MANAN <mrmanan5657@gmail.com>
2021-06-09 13:31:33 +0500

    added date and time

--- js.js
+++ js.js
@@ -1,6 +1,10 @@
 var btn = document.getElementById('btn');
 btn.onclick = function () {
   document.getElementById('new').style.fontFamily = "arial";
-  document.getElementById('new').innerHTML = "MR Manan class bscs.";
+  document.getElementById('new').innerHTML = "MR Manan class bscs";
 
+}
+var btn2 = document.getElementById("btn2");
+btn2.onclick = function () {
+  document.getElementById('date').innerHTML = Date();
 }
