8dc1c77a2a04020b31305c84ba660409f4b4568e
MR MANAN <mrmanan5657@gmail.com>
2021-06-09 12:37:20 +0500

    innerHTML added

--- js.js
+++ js.js
@@ -1,4 +1,6 @@
-var btn=document.getElementById('btn');
-btn.onclick=function(){
-  document.getElementById('new').style.color="blue";
+var btn = document.getElementById('btn');
+btn.onclick = function () {
+  document.getElementById('new').style.fontFamily = "arial";
+  document.getElementById('new').innerHTML = "MR Manan class bscs.";
+
 }
